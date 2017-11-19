e4f0717654677909088705fc56c978c2f75aaefc
Shun Git <kwokshungit@gmail.com>
2017-11-19 11:34:18 +0800

    update tests

--- common/tests.py
+++ common/tests.py
@@ -16,36 +16,62 @@ class SQLTestCase(TestCase):
 
     def test_single(self):
         result = sql('SELECT id FROM auth_user')
-        data = {'data': tuple((d[0],) for d in self.users)}
-        self.assertEqual(result, data)
+        self.assertEqual(result, tuple((d[0],) for d in self.users))
 
     def test_multi(self):
         result = sql('SELECT id, username FROM auth_user')
-        data = {'data': self.users}
-        self.assertEqual(result, data)
+        self.assertEqual(result, self.users)
+
+    def test_empty(self):
+        result = sql('SELECT id FROM auth_user WHERE id = 0')
+        self.assertEqual(result, ())
+
+    def test_aggregate(self):
+        result = sql('SELECT MAX(is_superuser) FROM auth_user')
+        self.assertEqual(result, ((1,),))
 
     def test_param(self):
         result = sql('SELECT username FROM auth_user WHERE id = %s', 1)
-        data = {'data': (('admin',),)}
-        self.assertEqual(result, data)
+        self.assertEqual(result, (('admin',),))
+
+    def test_insert(self):
+        q = """INSERT INTO auth_user (id, is_superuser, is_staff, is_active,
+                date_joined, username, password, email, first_name, last_name)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        result = sql(q, 6, 0, 0, 1, '2017-11-11', 'test4', '', '', '', '')
+        self.assertEqual(result, ())
 
 
 class SQLPageTestCase(TestCase):
 
     def test_page_default(self):
-        self.assertEqual(page(), ' LIMIT 20 OFFSET 0')
+        result = page()
+        self.assertEqual(result, ' LIMIT 20 OFFSET 0')
 
     def test_page_three(self):
-        self.assertEqual(page(page=3), ' LIMIT 20 OFFSET 40')
+        result = page(page=3)
+        self.assertEqual(result, ' LIMIT 20 OFFSET 40')
 
     def test_page_unlimited(self):
-        self.assertEqual(page(per_page=0), '')
+        result = page(per_page=0)
+        self.assertEqual(result, '')
 
     def test_page_sort(self):
-        self.assertEqual(page(per_page=0, sort=('id',)), ' ORDER BY id')
+        result = page(per_page=0, sort=('id',))
+        self.assertEqual(result, ' ORDER BY id')
 
     def test_page_sort_reverse(self):
-        self.assertEqual(page(per_page=0, sort=('-id',)), ' ORDER BY id DESC')
+        result = page(per_page=0, sort=('-id',))
+        self.assertEqual(result, ' ORDER BY id DESC')
+
+    def test_page_sort_multiple(self):
+        result = page(per_page=0, sort=('id', 'a', 'b'))
+        self.assertEqual(result, ' ORDER BY id, a, b')
+
+    def test_page_sort_multiple_with_reverse(self):
+        result = page(per_page=0, sort=('id', '-a'))
+        self.assertEqual(result, ' ORDER BY id, a DESC')
 
     def test_page_sort_default(self):
-        self.assertEqual(page(sort=('id',)), ' ORDER BY id LIMIT 20 OFFSET 0')
+        result = page(sort=('id',))
+        self.assertEqual(result, ' ORDER BY id LIMIT 20 OFFSET 0')
