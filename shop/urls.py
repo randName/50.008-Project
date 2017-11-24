c08c401559784ddf5f97a22f0d43c5b2dce7c138
Shun Git <kwokshungit@gmail.com>
2017-11-24 10:28:57 +0800

    add item views

--- shop/urls.py
+++ shop/urls.py
@@ -4,9 +4,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^items/$', views.item, name='item'),
     url(r'^item/rate/$', views.rate, name='rate'),
     url(r'^item/search/$', views.search, name='search'),
     url(r'^item/feedback/$', views.feedback, name='feedback'),
     url(r'^item/recommends/$', views.recommends, name='recommends'),
     url(r'^item/(?P<item_id>\d+)/$', views.item, name='item'),
+    url(r'^(?P<e>c(ompany|reator|ategory))s/$', views.entity, name='entitys'),
+    url(r'^(?P<e>c(ompany|reator|ategory))/(?P<e_id>\d+)$',
+        views.entity, name='entity'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from common.db import sql, page
+from common.utils import pagination
 from common.decorators import json_response
 
 
@@ -9,9 +11,58 @@ def index(request):
 
 
 @json_response
-def item(request, item_id):
+def entity(request, e, e_id=None):
+    """Get entity details."""
+    if e_id:
+        q = 'SELECT name FROM {} WHERE id = %s'.format(e)
+        try:
+            return {
+                'id': e_id,
+                'name': sql(q, int(e_id))[0][0],
+            }
+        except IndexError:
+            return None
+
+    q = 'SELECT id, name FROM {}'.format(e)
+    pg = pagination(request)
+
+    return ({'id': i[0], 'name': i[1]} for i in sql(q + page(**pg)))
+
+
+@json_response
+def item(request, item_id=None):
     """Get item details."""
-    return {}
+    if item_id:
+        q = """SELECT name, price, quantity, description, date_created, company_id
+                FROM item WHERE id = %s"""
+        try:
+            it = sql(q, item_id)[0]
+        except IndexError:
+            return None
+
+        q = """SELECT {0}.id, {0}.name FROM item_{0}
+                INNER JOIN {0} ON item_{0}.{0}_id = {0}.id
+                WHERE item_id = %s"""
+        cs = {}
+        for k in ('creator', 'category'):
+            c = sql(q.format(k), item_id)
+            cs[k+'s'] = tuple({'id': i[0], 'name': i[1]} for i in c)
+
+        return {
+            **cs,
+            'id': item_id,
+            'name': it[0],
+            'price': it[1],
+            'quantity': it[2],
+            'description': it[3],
+            'date_created': it[4],
+            'company': entity.__wrapped__(None, 'company', it[5]),
+        }
+
+    q = 'SELECT id, name FROM item'
+    pg = pagination(request)
+
+    return ({'id': i[0], 'name': i[1]} for i in sql(q + page(**pg)))
 
 
 @json_response
