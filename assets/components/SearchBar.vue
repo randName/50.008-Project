4023ecdda88dfc8f16c9586ff5c82ed2991db8db
Shun Git <kwokshungit@gmail.com>
2017-12-06 09:01:23 +0800

    add searchbar

---
+++ assets/components/SearchBar.vue
@@ -0,0 +1,75 @@
+<template>
+<v-toolbar app fixed flat ref="toolbar" :manual-scroll="!searching">
+  <v-btn icon large @click="nosearch">
+    <v-icon>arrow_back</v-icon>
+  </v-btn>
+  <v-text-field clearable single-line solo light
+    id="search" key="search" ref="search" v-model="search"
+    placeholder="Search" prepend-icon="search" @keyup.enter="dosearch"
+  ></v-text-field>
+  <v-menu offset-y v-model="showsearch" activator="#search">
+    <v-list light>
+      <v-list-tile v-for="i in suggestions" :key="i.type+i.id" @click="dosearch(i)">
+        <v-list-tile-title>{{ i.name }}</v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-menu>
+</v-toolbar>
+</template>
+
+<script>
+export default {
+  props: ['searching'],
+  computed: {
+    showsearch: {
+      get () {
+        return this.searching && this.suggestions.length > 0
+      },
+      set (v) {
+        if (!v) {
+          this.nosearch()
+        }
+      }
+    },
+  },
+  watch: {
+    searching (val) {
+      this.$refs.toolbar.isScrolling = !val
+      if (val) {
+        this.$nextTick(() => this.$refs.search.focus())
+      } else {
+        this.search = null
+        this.suggestions = []
+      }
+    },
+    search (val) {
+      if (val && val.length > 3) {
+        console.log(val)
+      }
+    },
+  },
+  methods: {
+    nosearch () {
+      this.$emit('nosearch')
+    },
+    dosearch (entity) {
+      let query = {}
+      if (entity.name) {
+        query[entity.type] = entity.id
+      } else if (this.search) {
+        query = {q: this.search}
+      }
+      if (Object.keys(query).length) {
+        this.nosearch()
+        this.$router.push({name: 'Search', query})
+      }
+    }
+  },
+  data () {
+    return {
+      search: null,
+      suggestions: [],
+    }
+  }
+}
+</script>
